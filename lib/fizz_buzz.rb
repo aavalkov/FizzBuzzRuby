7ed1c31fc85c794d343b60cba62834b2d4d0e48c
Anna Valkov & Michael Derfler <student@epicodus.com>
2014-07-21 11:07:57 -0700

    will count up from zero to whatever number the user inputs

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,15 +1,22 @@
 number = gets.chomp.to_i
 def fizz_buzz(number)
-  if number % 3 == 0 && number % 5 == 0
-    number = "fizzbuzz"
-    "fizzbuzz"
-  elsif number % 3 == 0
-    number = "fizz"
-    puts "fizz"
-    "fizz"
-  elsif number % 5 == 0
-    number = "buzz"
-    "buzz"
-  end
+  array = []
+    while number > 0
+      if number % 3 == 0 && number % 5 == 0
+        array.push("fizzbuzz")
+        print "fizzbuzz"
+      elsif number % 3 == 0
+        array.push("fizz")
+        print "fizz"
+      elsif number % 5 == 0
+        array.push("buzz")
+        "buzz"
+      else
+        array.push(number)
+        print number
+      end
+        number -= 1
+    end
+  array.reverse
 end
 fizz_buzz(number)
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -4,12 +4,15 @@ require("fizz_buzz")
 
 describe("fizz_buzz") do
   it("changes 3 to 'fizz'") do
-    fizz_buzz(3).should(eq("fizz"))
+    fizz_buzz(3).should(eq([1,2,"fizz"]))
   end
   it("changes 5 to 'buzz'") do
-    fizz_buzz(5).should(eq("buzz"))
+    fizz_buzz(5).should(eq([1,2,"fizz",4,"buzz"]))
   end
   it("changes 15 to fizzbuzz") do
-    fizz_buzz(15).should(eq("fizzbuzz"))
+    fizz_buzz(15).should(eq([1,2,"fizz", 4, "buzz","fizz",7,8,"fizz","buzz",11,"fizz",13,14,"fizzbuzz"]))
+  end
+  it("does not change numbers not divisible by 3 or 5") do
+    fizz_buzz(1).should(eq([1]))
   end
 end
